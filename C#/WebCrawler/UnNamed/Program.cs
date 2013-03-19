99765547dff7cedf867864c23b7466ff56773084
Darian <darian.everett@gmail.com>
2013-03-18 20:22:08 -0600

    Beginning WebCrawler project Old calculator project also added

---
+++ C#/WebCrawler/UnNamed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\input\text.txt";
+            string URL = "http://en.wikipedia.org/wiki/Main_Page";
+            WebConnection.GetFile(URL, path);
+        }
+
+
+
+    }
+}
---
+++ C#/WebCrawler/UnNamed/WebConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    class WebConnection
+    {
+        public static void GetFile(string strURL, string strFilePath)
+        {
+            WebRequest myWebRequest = WebRequest.Create(strURL);
+            WebResponse myWebResponse = myWebRequest.GetResponse();
+            Stream ReceiveStream = myWebResponse.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader reader = new StreamReader(ReceiveStream, encode);
+            string strResponse = reader.ReadToEnd();
+            StreamWriter writer = new StreamWriter(strFilePath);
+            writer.WriteLine(strResponse);
+            writer.Close();
+            reader.Close();
+            myWebResponse.Close();
+        }
+    }
+}
