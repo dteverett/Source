64aa587677e27ff2c489f0d4431809582728a3fd
deverett <deverett@apexedi.com>
2013-03-20 15:48:04 -0600

    LINQ to SQL

---
+++ C#/DatabaseConnection/DatabaseConnection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace DatabaseConnection
+{
+
+    //[Table(UriHostNameType = "Tests")]
+    //public class Tests
+    //{
+    //    private int _logID;
+    //    [Column(IsPrimaryKeytrue, Storage = "_logID")]
+    //    public int LogID
+    //    {
+    //        get { return this._logID; }
+    //        set { this._logID = value; }
+    //    }
+
+    //    private string _DateCreated_DT;
+    //    [Column(Storage = "_DateCreated_DT")]
+    //    public string DateCreated_DT
+    //    {
+    //        get { return this._DateCreated_DT; }
+    //        set { this._DateCreated_DT = value; }
+    //    }
+    //}
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connection connect = new Connection();
+
+            //Alternative Connection
+            //DataContext db = new DataContext (@"C:\linqtest5\northwnd.mdf");
+
+            
+            
+        }
+    }
+}
